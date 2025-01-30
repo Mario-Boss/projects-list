ee4e9a69e083d2010bd956fff41545da023cc3f3
Mario Boss <ask@mario-boss.com>
2025-01-30 20:35:40 +0100

    injected FavoriteProjects component to the left sidebar in the projects layout

--- src/app/projects/layout.jsx
+++ src/app/projects/layout.jsx
@@ -1,7 +1,12 @@
+import FavoriteProjects from "@/components/FavoriteProjects";
+
 export default async function ProjectsLayout({ children }) {
   return (
     <>
-      <div className="flex-none pr-8 w-auto border-r-2 border-silver">Favorite Projects</div>
+      <div className="flex-none pr-8 w-auto border-r-2 border-silver">
+        <h2 className="font-bold uppercase text-lg">Favorite Projects</h2>
+        <FavoriteProjects />
+      </div>
       <div className="grow pl-8">{children}</div>
     </>
   );
