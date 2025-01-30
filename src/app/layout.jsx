9265aeb3151ef483b77ef0d352ec11ec3995bc7d
Mario Boss <ask@mario-boss.com>
2025-01-30 19:32:53 +0100

    moved layout code from page to the main layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,12 @@
 import { ProjectProvider } from "@/providers/project";
-import { getProjects } from '@/data/projects';
+import { getProjects } from "@/data/projects";
+import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,14 +23,22 @@ export const metadata = {
   description: "Project made by Mariusz Andryszczuk to show frontend skills by building projects list"
 };
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   let projectPromise = getProjects;
 
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ProjectProvider projectPromise={projectPromise}>
-          {children}
+          <div className="flex flex-col justify-items-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex w-full grow">{children}</main>
+            <footer className="flex w-full items-center justify-center">
+              <a className="flex items-center gap-4 hover:underline hover:underline-offset-4" href="https://mario-boss.com" target="_blank" rel="noopener noreferrer">
+                <Image aria-hidden src="/logo.svg" alt="Mariusz Andryszczuk" width={60} height={60} />
+                <span>Made by Mariusz Andryszczuk</span>
+              </a>
+            </footer>
+          </div>
         </ProjectProvider>
       </body>
     </html>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,13 @@
-'use client'
- 
-import { useProject } from '@/providers/project';
-import Image from "next/image";
+"use client";
+import Link from "next/link";
+import { Button } from "@mui/material";
 
 export default function Home() {
-  const { state, dispatch } = useProject();
-  const favoritedProjects = state.projects.filter(project => project.favorite);
-  console.log("ALL PROJECTS:", state.projects);
-  console.log("FAVORITED PROJECTS:", favoritedProjects);
-
   return (
-    <div className="flex flex-col justify-items-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex w-full grow">
-        <div className="flex-none mr-8 w-auto">SIDEBAR</div>
-        <div className="grow">MAIN AREA</div>
-      </main>
-      <footer className="flex w-full items-center justify-center">
-        <a className="flex items-center gap-4 hover:underline hover:underline-offset-4" href="https://mario-boss.com" target="_blank" rel="noopener noreferrer">
-          <Image aria-hidden src="/logo.svg" alt="Mariusz Andryszczuk" width={60} height={60} />
-          <span>Made by Mariusz Andryszczuk</span>
-        </a>
-      </footer>
+    <div className="flex self-center w-full justify-center">
+      <Link href="/projects">
+        <Button variant="contained">PROJECTS</Button>
+      </Link>
     </div>
   );
 }
