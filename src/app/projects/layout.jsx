2fbf03fe7ea92a90b46ca38594d628e7f6807df2
Mario Boss <ask@mario-boss.com>
2025-02-01 08:34:41 +0100

    injected PageTitle component to the projects layout and decision to add dynamic title page based on the routing

--- src/app/projects/layout.jsx
+++ src/app/projects/layout.jsx
@@ -1,13 +1,30 @@
-import FavoriteProjects from "@/components/FavoriteProjects";
+"use client";
 
-export default async function ProjectsLayout({ children }) {
+import { useSelectedLayoutSegments } from "next/navigation";
+import FavoriteProjects from "@/components/FavoriteProjects";
+import PageTitle from "@/components/PageTitle";
+
+export default function ProjectsLayout({ children }) {
+  const urlSegments = useSelectedLayoutSegments();
+  let title = "Projects Manager";
+  if (urlSegments[0] === "new") {
+    title = "Adding a New Project";
+  } else if (urlSegments.length === 1) {
+    title = "Viewing Project Details";
+  } else if (urlSegments[1] === "edit") {
+    title = "Editing Project Data";
+  }
+
   return (
     <>
       <div className="flex-none pr-8 w-auto border-r-2 border-silver">
-        <h2 className="font-bold uppercase text-lg">Favorite Projects</h2>
+        <PageTitle title="Favorite Projects" variant="h2" />
         <FavoriteProjects />
       </div>
-      <div className="grow pl-8">{children}</div>
+      <div className="grow pl-8">
+        <PageTitle title={title} variant="h1" />
+        {children}
+      </div>
     </>
   );
 }
