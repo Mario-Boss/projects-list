7fc24bcfc2f3b2a6f388595d5b38285f3e7852fe
Mario Boss <ask@mario-boss.com>
2025-02-01 07:47:17 +0100

    added structure updates to the ProjectButton component to remove the problem with full width clickable row

--- src/components/ProjectButton.js
+++ src/components/ProjectButton.js
@@ -1,19 +1,15 @@
 "use client";
 
 import Link from "next/link";
-import { styled } from "@mui/material/styles";
 import { Button } from "@mui/material";
-
-const StyledButton = styled(Link)(() => ({
-  display: "flex",
-  justifyContent: "flex-end",
-  marginBottom: "32px"
-}));
+import Grid from "@mui/material/Grid2";
 
 export default function ProjectsTableHead() {
   return (
-    <StyledButton href="/projects/new">
-      <Button variant="contained">Create Project</Button>
-    </StyledButton>
+    <Grid align="right" sx={{ mb: 4 }}>
+      <Link href="/projects/new">
+        <Button variant="contained">Create Project</Button>
+      </Link>
+    </Grid>
   );
 }
