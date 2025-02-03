a97aa6b66f5c5b678596b027d59bd2d29655433e
Mario Boss <ask@mario-boss.com>
2025-02-03 10:09:46 +0100

    set dataSource env constant to "local" to start the app with local mockup data instead of "express" which will serve data through Express endpoint and requires express server to be launched

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        dataSource: 'express', // posible values: "local", "express"
+        dataSource: 'local', // posible values: "local", "express"
         expressAPIsource: "http://localhost:3003/api" // when using "express" value above, please run command "npm run server" in separated terminal to start express.js server
     }
 };
