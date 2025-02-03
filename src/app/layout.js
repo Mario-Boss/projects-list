a78846eccf234d4c015b1ee16eea1336aa958f1f
Mario Boss <ask@mario-boss.com>
2025-02-03 11:47:03 +0100

    added next progress bar module and injected it into the layout to show responding for all navigation actions

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@
 import { ProjectProvider } from "@/providers/project";
 import { SnackbarProvider } from "@/providers/snackbar";
 import { getProjects } from "@/data/projects";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import Image from "next/image";
 import "./globals.css";
 import "@fontsource/roboto/300.css";
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
           <ProjectProvider projectPromise={projectPromise}>
             <div className="flex w-full flex-col justify-items-center min-h-screen p-4 lg:p-8">
               <main className="flex w-full grow flex-col lg:flex-row">{children}</main>
+              <ProgressBar height="4px" color="#9c27b0" options={{ showSpinner: false }} shallowRouting />
               <footer className="flex w-full items-center justify-center">
                 <a className="flex items-center gap-4 hover:underline hover:underline-offset-4" href="https://mario-boss.com" target="_blank" rel="noopener noreferrer">
                   <Image aria-hidden src="/logo.svg" alt="Mariusz Andryszczuk" width={60} height={60} style={{ width: "60px", height: "60px" }} />
